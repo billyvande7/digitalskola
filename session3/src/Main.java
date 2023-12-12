b0eeea62b9321c3be4c14d534e36603d20a904e0
billyvande7 <billyvande7@gmail.com>
2023-12-12 20:28:21 +0700

    add: session3 homework

---
+++ session3/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String bintang = "";
+        for (int i = 0; i<10; i++){
+            if(i < 5){
+                bintang += "*";
+                System.out.println(bintang);
+            } else if (i > 5) {
+                System.out.println(bintang.substring(0, 5 - (i - 5)));
+            } else{
+                System.out.println(bintang);
+            }
+        }
+    }
+}
